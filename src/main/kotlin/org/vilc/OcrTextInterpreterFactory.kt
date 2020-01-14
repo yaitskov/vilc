48fc9e256cbd3247ac26337ed583f76e200fed1e
Daniil Iaitskov <rtfm.rtfm.rtfm@gmail.com>
2020-01-14 12:00:50 -0800

    master handle query params

--- src/main/kotlin/org/vilc/OcrTextInterpreterFactory.kt
+++ src/main/kotlin/org/vilc/OcrTextInterpreterFactory.kt
@@ -5,7 +5,10 @@ import java.util.regex.Pattern
 object OcrTextInterpreterFactory {
 
   fun create(): OcrTextInterpreter {
+    val host = """^.*[ /](([A-Za-z0-9_-]+[,.])+(org|ru|pl|biz|com|net|mil|gov|us)([;:][0-9]{2,5} ?)?"""
+    val path = """([/][A-Za-z0-9_+%-]+)*"""
+    val params = """/?([?&][A-Za-z0-9_-]+(=[A-Za-z0-9_.,%+-]+)?)*).*$"""
     return OcrTextInterpreter(
-        Pattern.compile("""^.*[ /](([A-Za-z0-9_-]+[.])+(org|ru|pl|biz|com|net|mil|gov|us)(/[A-Za-z0-9_+%-]+)*).*$"""))
+        Pattern.compile(host + path + params))
   }
 }
--- src/test/kotlin/org/vilc/OcrTextInterpreterTest.kt
+++ src/test/kotlin/org/vilc/OcrTextInterpreterTest.kt
@@ -13,4 +13,32 @@ class OcrTextInterpreterTest {
         "X @ U a https://medium.com/fuzzy-sharp/purescript-and-haskell-at-lumi-7e852b16fb13\n"),
         Is.`is`(Optional.of(URL("http://medium.com/fuzzy-sharp/purescript-and-haskell-at-lumi-7e852b16fb13"))))
   }
+
+  @Test
+  fun withPort() {
+    assertThat(OcrTextInterpreterFactory.create().extractUrl(
+        "X @ U a https://foodfest.net:8443/festival/kelner/serve/order/233/E1\n"),
+        Is.`is`(Optional.of(URL("http://foodfest.net:8443/festival/kelner/serve/order/233/E1"))))
+  }
+
+  @Test
+  fun withParams() {
+    assertThat(OcrTextInterpreterFactory.create().extractUrl(
+        "X @ U a https://www.google.com/search?client=firefox-b-1-d&q=who+calls+liftShowsPrec\n"),
+        Is.`is`(Optional.of(URL("http://www.google.com/search?client=firefox-b-1-d&q=who+calls+liftShowsPrec"))))
+  }
+
+  @Test
+  fun stackFromChrome() {
+    assertThat(OcrTextInterpreterFactory.create().extractUrl(
+        "C' @ stackoverflow.com/questions/10967451/open-a-link-in-browser-with-java-button\n"),
+        Is.`is`(Optional.of(URL("http://stackoverflow.com/questions/10967451/open-a-link-in-browser-with-java-button"))))
+  }
+
+  @Test
+  fun stackGithub() {
+    assertThat(OcrTextInterpreterFactory.create().extractUrl(
+        "< C' @ https;/github.expedia.biz/Brand-Expedia/Ins-templates/pull/320/files "),
+        Is.`is`(Optional.of(URL("http://github.expedia.biz/Brand-Expedia/Ins-templates/pull/320/files"))))
+  }
 }
